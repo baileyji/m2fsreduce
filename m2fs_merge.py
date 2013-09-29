e46cea86b1bb1792dc33072f8f5cf12474ec6255
Jeb Bailey <baileyji@umich.edu>
2013-09-29 14:14:34 -0400

    m2fs_merge now works

--- m2fs_merge.py
+++ m2fs_merge.py
@@ -1,15 +1,16 @@
 #from numpy import *
 import numpy as np
 from astropy.io import fits
+import re
+from pylab import *
+import astropy.stats
 
 
-#def mergeimage(frameno,side=None):
-    frameno=84
-    side='b'
+def mergeimage(frameno,side=None):
     if side == None:
         mergeimage(frameno,side='r')
         mergeimage(frameno,side='b')
-        return
+        #return
     basename=side+'{frameno:04}'
     f=basename+'c{quad}.fits'
 
@@ -27,30 +28,38 @@ from astropy.io import fits
     crop=[int(s) for s in re.findall(r'\d+',trimsec)]
 
     #Create an output array
-    out=np.zeros((crop[1]*2,crop[3]*2),dtype=np.int16)
+    out=np.zeros((crop[3]*2,crop[1]*2),dtype=np.int16)
 
     #Define where the quadrants show go
-    quadLoc=[(0, 2048, 0, 2056),
-             (2048, 4096, 0,2056),
-             (2048, 4096, 2056, 4112),
-             (0, 2048, 2056, 4112)]
+    quadLoc=[(0, 2056, 0, 2048),
+            (0,2056, 2048, 4096),
+             ( 2056, 4112, 2048, 4096),
+             (2056, 4112,0, 2048)]
 
     #Create a list of the quadrant's data to loop through
     quadrantData=[quadrant1[0].data, quadrant2[0].data,
-                 quadrant2[0].data, quadrant4[0].data]
+                 quadrant3[0].data, quadrant4[0].data]
 
     for i, qdata in enumerate(quadrantData):
 
         #Compute mean for overscan region rowwise
-        biaslevels=np.mean(qdata[crop[0]-1:crop[1],bias[2]-1:bias[3]],axis=1)
+        biaslevels=np.mean(qdata[crop[2]-1:crop[3],bias[0]-1:bias[1]],axis=1)
 
         #Crop image & subtract bias levels row wise
-        qdata=(qdata[crop[0]-1:crop[1],crop[2]-1:crop[3]] -
+        qdata=(qdata[crop[0]-1:crop[3],crop[0]-1:crop[1]] -
                           biaslevels[:,np.newaxis])
 
-
-        out[quadLoc[i][0]:quadLoc[i][1],quadLoc[i][2]:quadLoc[i][3]]=qdata
-
+        if i ==0:
+            out[quadLoc[i][0]:quadLoc[i][1],quadLoc[i][2]:quadLoc[i][3]]=qdata
+        if i==1:
+            out[quadLoc[i][0]:quadLoc[i][1],
+                quadLoc[i][2]:quadLoc[i][3]]=np.fliplr(qdata)
+        if i==2:
+            out[quadLoc[i][0]:quadLoc[i][1],
+                quadLoc[i][2]:quadLoc[i][3]]=np.rot90(qdata,2)
+        if i==3:
+            out[quadLoc[i][0]:quadLoc[i][1],
+                quadLoc[i][2]:quadLoc[i][3]]=np.rot90(np.fliplr(qdata),2)
 
     #Write out the merged file
     hdu = fits.PrimaryHDU(out)
@@ -62,54 +71,75 @@ from astropy.io import fits
     hdu.writeto(basename+'.fits')
 
 
+def makesuperbias(filenos, side, name):
 
-def makesuperflat(files, name, superbias=None):
-    #load first flat to get info
-    with fits.open(files[0]) as flat
-        pass
+    f=side+'{frameno:04}.fits'
+
+    #load first bias to get info
+    with fits.open(f.format(frameno=filenos[0])) as bias:
+        header=bias[0].header
+        superbias_data=np.zeros_like(bias[0].data,dtype=np.float32)
+
+    #Number of bias frames
+    nbias=len(filenos)
+
+    #Sum the bias counts    
+    for num in filenos:
+        with fits.open(f.format(frameno=num)) as bias:
+            superbias_data+=bias[0].data
+
+    #Merge bias
+    superbias_data/=nbias
+
+    #Write out the merged file
+    hdu = fits.PrimaryHDU(superbias_data)
+    hdu.header=header
+    hdu.header['FILENAME']=name
+    hdu.header['COMMENT']=','.join(map(str,filenos))
+    hdu.writeto(name+'.fits')
+
+
+
+def makesuperflat(filenos, side, name, superbias=None):
+
+    f=side+'{frameno:04}.fits'
     
-    #Number of flats
-    nflat=len(files)
+    #load first flat to get info
+    with fits.open(f.format(frameno=filenos[0])) as flat:
+        header=flat[0].header
+        superflat_data=np.zeros_like(flat[0].data,dtype=np.float32)
+        nrow,ncol=superflat_data.shape
+    
+    #Number of flat frames
+    nflat=len(filenos)
+    
+    
+    #Grab the bias
+    try:
+        biasdata=fits.open(superbias)[0].data
+    except IOError:
+        biasdata=None
 
     #Make the datacube
-    cube=np.zeros((nrow,ncol, nflats), dtype=np.float32)
-
-    for f in enumerate(files):
-        with fits.open(f) as flat:
-        
-            #Remove CRs & put in cube
-            
-            cube[:,:,i]=sigmaClip(flat[0].data)
+    cube=np.zeros((nrow, ncol, nflat), dtype=np.float32)
+    
+    
+    for i,num in enumerate(filenos):
+        with fits.open(f.format(frameno=num)) as flat:
+            #Remove bias
+            if biasdata != None:
+                cube[:,:,i]=flat[0].data-biasdata
+            else:
+                cube[:,:,i]=flat[0].data
 
     #Merge flat
+    masked=astropy.stats.sigma_clip(cube,sig=3,axis=2)
+    superflat_data=np.ma.MaskedArray.mean(masked,axis=2).data
 
-    #Subtract the superbias from the superflat
-    try:
-        with fits.open(superbias) as bias:
-               flat.data-=bias[0].data
-    except IOError:
-        pass
-        
     #Write out the superflat
-    hdu.header['COMMENT']=''.join([)
-    hdu.writeto(name)
+    hdu = fits.PrimaryHDU(superflat_data)
+    hdu.header=header
+    hdu.header['FILENAME']=name
+    hdu.header['COMMENT']=','.join(map(str,filenos))+',Bias:'+str(superbias)
+    hdu.writeto(name+'.fits')
 
-
-def makesuperbias(files, name):
-    #load first bias to get info
-    with fits.open(files[0]) as bias
-        pass
-    
-    #Number of bias frames
-    nbias=len(files)
-    
-    #Make the datacube
-    cube=np.zeros((nrow,ncol, nbias), dtype=np.int16)
-    
-    for f in enumerate(files):
-        with fits.open(f) as bias:
-            cube[:,:,i]=bias[0].data
-    
-    #Merge bias
-
-    hdu.writeto(name)
